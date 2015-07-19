d1d7ac7b2c8c92b43e7a2790c2b7747a19532e0e
lyk001 <649287196@qq.com>
2015-07-19 21:08:20 +0800

    7.19 更新了homeactivity
    把主页设计了一下，然后把更新了各种id
    rip_personal
    rip_account
    rip_exchange
    rip_setting

--- YeS/src/com/exchange/yes/HomeActivity.java
+++ YeS/src/com/exchange/yes/HomeActivity.java
@@ -1,12 +1,13 @@
 package com.exchange.yes;
 
-import com.exchange.yes.R;
-import com.exchange.yes.R.layout;
-import com.exchange.yes.R.menu;
-
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+
+import com.gc.materialdesign.views.LayoutRipple;
 
 public class HomeActivity extends Activity {
 
@@ -14,6 +15,51 @@ public class HomeActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_home);
+		
+		 LayoutRipple Rip_personal = (LayoutRipple) findViewById(R.id.rip_personal);
+
+	        Rip_personal.setOnClickListener(new OnClickListener() {
+				
+				@Override
+				public void onClick(View arg0) {
+					Intent intent = new Intent(HomeActivity.this,ButtonsActivity.class);
+					startActivity(intent);
+				}
+			});
+		
+	        LayoutRipple Rip_account = (LayoutRipple) findViewById(R.id.rip_account);
+
+	        Rip_account.setOnClickListener(new OnClickListener() {
+				
+				@Override
+				public void onClick(View arg0) {
+					Intent intent = new Intent(HomeActivity.this,ButtonsActivity.class);
+					startActivity(intent);
+				}
+			});
+	        
+	        LayoutRipple Rip_exchange = (LayoutRipple) findViewById(R.id.rip_exchange);
+
+	        Rip_exchange.setOnClickListener(new OnClickListener() {
+				
+				@Override
+				public void onClick(View arg0) {
+					Intent intent = new Intent(HomeActivity.this,ButtonsActivity.class);
+					startActivity(intent);
+				}
+			});
+	        
+	        LayoutRipple Rip_setting = (LayoutRipple) findViewById(R.id.rip_setting);
+
+	        Rip_setting.setOnClickListener(new OnClickListener() {
+				
+				@Override
+				public void onClick(View arg0) {
+					Intent intent = new Intent(HomeActivity.this,ButtonsActivity.class);
+					startActivity(intent);
+				}
+			});
+	        
 	}
 
 	@Override
